80ae54cda2e18d95a8300a0dfdf2372c77d75506
lizcauffiel <Tinkwyatt811!>
2018-10-24 15:33:59 -0400

    works with node.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,40 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.setHeader('index', { title: 'Moodsetter' });
-	res.use(express.static('css'));
-	res.end();
+	res.render('index', { title: 'Moodsetter' });
+	// res.use(express.static('css'));
+	// res.end();
+});
+
+// nav bar
+router.get('/presetcolor', function (req, res, next) {
+	res.render('presetcolor', { page: 'PreSet Colors', menuId: 'presetcolor' });
+});
+router.get('/moodcolor', function (req, res, next) {
+	res.render('moodcolor', { page: 'Mood Colors', menuId: 'moodcolor' });
+});
+router.get('/mycolors', function (req, res, next) {
+	res.render('mycolors', { page: 'My Colors', menuId: 'mycolors' });
+});
+router.get('/faq', function (req, res, next) {
+	res.render('faq', { page: 'FAQ', menuId: 'faq' });
+});
+router.get('/about', function (req, res, next) {
+	res.render('about', { page: 'About Us', menuId: 'about' });
+});
+router.get('/contact', function (req, res, next) {
+	res.render('contact', { page: 'Contact Us', menuId: 'contact' });
+});
+
+// footer
+router.get('/faq', function (req, res, next) {
+	res.render('faqFooter', { page: 'FAQ', menuId: 'faqFooter' });
+});
+router.get('/about', function (req, res, next) {
+	res.render('aboutFooter', { page: 'About', menuId: 'aboutFooter' });
+});
+router.get('/contact', function (req, res, next) {
+	res.render('contactFooter', { page: 'Contact', menuId: 'contactFooter' });
 });
 
 module.exports = router;
